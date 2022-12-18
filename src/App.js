9e47c589520aa504949a48b12a2fdfc7f3eabe0e
NarekHakobyanJS <hakobyan95narek@gmail.com>
2022-12-18 15:38:14 +0400

    one lesson

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
 function App() {
+  const { register, handleSubmit, formState : {errors}, reset } = useForm()
+
+  const onSubmit = (data) => {
+    alert(`Your name ${data.name}`)
+    console.log(data)
+    reset()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...(register('name', {
+            required : "Name is requerd"
+          }))}
+          type="text"
+        />
+        {errors?.name && <div style={{color : "red"}}>{errors.name.message}</div>}
+
+        <input
+          {...(register('email', {
+            required : "Email is requerd",
+            pattern : {
+              value : /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message : 'Plase enter Valid email'
+            }
+          }))}
+          type="text"
+        />
+        {errors?.email && <div style={{color : "red"}}>{errors.email.message}</div>}
+
+        <div><button>send</button></div>
+      </form>
     </div>
   );
 }
